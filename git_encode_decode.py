3429e4d627c1bf2d7208ce5adee7613dd1f9bb5d
Alan Pham <alanpham@ufl.edu>
2023-10-25 13:38:51 -0400

    Alan adding encode and main function

---
+++ git_encode_decode.py
@@ -0,0 +1,22 @@
+def main():
+    encoded = None
+    while True:
+        print('Menu \n ------------- \n 1. Encode \n 2. Decode \n 3. Quit')
+        print()
+        option = input('Please enter an option: ')
+        if option == '1':
+            password = input('Please enter your password to encode: ')
+            encoded = encode(password)
+        elif option == '2':
+            decoded = decode(encoded)
+            print('The encoded password is '+ encoded + ' and the original password is ' + decoded + ".")
+        elif option == '3':
+            break
+def encode(password):
+    print('Your password has been stored')
+    for item in password:
+        temp = int(item) + 3
+        new += str(temp)
+
+    return new
+def decode(encoded)
